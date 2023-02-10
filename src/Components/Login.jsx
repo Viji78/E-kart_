79690fed1066d60ae2228796635ae595a22c5d65
Viji78 <vgms358@gmail.com>
2023-02-10 19:31:08 +0530

    Small_Changed

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,14 +6,19 @@ const Login = () => {
 
     return (
         <>
+         <div className="hero">
+            <div className="card bg-dark text-black border-0">
+                <img src="/assets/background_image3.jpg" className="card-img" alt="BackgroundImage" height="550px" />
+                <div className="card-img-overlay d-flex flex-column justify-content-center ">
+                    
             <div id="fromdiv">
                 <form action="./" className="rounded p-4 p-sn-3 " id="formdesign">
-                    <table>
+                    <table cellPadding={"20px"} >
                         <tr className="mb-r3">
                             <td> <label htmlFor=""><b>Email Address:</b></label></td>
                             <td><input type="email" /></td>
                         </tr>
-                        <tr className="mb-r3">
+                        <tr className="mb-r3" >
                             <td> <label htmlFor=""><b>Password:</b></label></td>
                             <td> <input type="password" /></td>
                         </tr>
@@ -27,6 +32,10 @@ const Login = () => {
                     </table>
                 </form>
             </div>
+
+            </div>
+            </div>
+        </div>
         </>
     );
 }
